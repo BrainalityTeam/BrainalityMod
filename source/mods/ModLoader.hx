bf7e16e79c8788a91e6c86b475f34518b5ab669b
Brainy0789 <bpurdy12@icloud.com>
2025-11-01 13:36:08 -0700

    updated imports

--- source/mods/ModLoader.hx
+++ source/mods/ModLoader.hx
@@ -1,4 +1,4 @@
-package;
+package mods;
 
 import sys.net.Address;
 import utils.SemVer;
--- tests/flixel/source/Main.hx
+++ tests/flixel/source/Main.hx
@@ -1,2 +1,10 @@
 package;
 
+import openfl.display.Sprite;
+import flixel.FlxGame;
+class Main extends Sprite
+{
+    static function main() {
+        addChild(new FlxGame(0, 0, PlayState));
+    }
+}
